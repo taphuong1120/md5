96cf23125b627f44faa5af0215ce6c5aa14f236e
DucNguyenND <ducnguyenbvtvc@gmail.com>
2022-08-25 23:46:47 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TourService} from "./service/tour.service";
+import {Tour} from "../models/Tour";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thimd5';
+
+  constructor(private tourservice:TourService) { }
+
+  ngOnInit(): void {
+  }
+
 }
---
+++ src/app/tour/show/show.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {TourService} from "../../service/tour.service";
+import {Tour} from "../../../models/Tour";
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.component.html',
+  styleUrls: ['./show.component.css']
+})
+export class ShowComponent implements OnInit {
+  tours:Tour[]=[]
+
+  constructor(private tourservice:TourService) { }
+
+  ngOnInit(): void {
+    this.tourservice.getall().subscribe((data)=>{
+      this.tours=data
+    })
+  }
+
+}
